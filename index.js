000a242168f9c7fc8ea0b05416fdc35fd738d52e
Kamal <kamal.raghe33@gmail.com>
2024-09-16 15:45:27 -0400

    commit

--- index.js
+++ index.js
@@ -2,12 +2,29 @@
     <input class='User' id="input" type="text" placeholder="Enter string" >  */}
     const string = document.getElementById('string')
     const re = document.getElementById('re')    
-    const str = 'kamal raghe'
-    const charMap = {}
+    let charMap = {}
+    const input = document.getElementById('input')
+    const btn = document.getElementById('btn')
     
-    for(let char of str){
-        charMap[char] = charMap[char] + 1 || 1
-    }
+    
   
+    function Max(){
+       let str = input.value
+       let MaxNum = 0
+       let MaxChar = ''
+       for(let char of str){
+            charMap[char] = charMap[char] + 1 || 1
+        }
+        str = ''      
+       for(let char in charMap){
+        if(charMap[char] > MaxNum){
+            MaxNum = charMap[char]
+            MaxChar = char
+            string.innerHTML = 'The letter ' + MaxChar + ' repeats the most'
+            re.innerHTML = "It repeats "+MaxNum + " times"
+        }
+       }charMap = {}  
+       
+    }
 
-    console.log(charMap)
+ btn.addEventListener('click', Max)
