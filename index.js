e9ba6e1dba51538c27911b6165ee3fe8db630699
Kamal <kamal.raghe33@gmail.com>
2024-09-16 15:13:06 -0400

    commit

--- index.js
+++ index.js
@@ -2,37 +2,16 @@
     <input class='User' id="input" type="text" placeholder="Enter string" >  */}
     const string = document.getElementById('string')
     const re = document.getElementById('re')    
-    const array = [
-        { id: 1, age: 25 },
-        { id: 2, age: 26 },
-        { id: 3, age: 26 },
-        { id: 2, age: 27 },
-    ]
+    const str = 'kamal raghe'
+    const charMap = {}
     
-    // function StringId(){
-    //     const idArray = array.map(obj=>obj.id)
-    //     const arr = []
-    //     let IdString = ''
-
-    //     for(let id of idArray){
-    //         if(!arr.includes(id)){
-    //             arr.push(id)
-                
-    //         }
-    //     }
-    //     for(let i = 0; i < arr.length; ++i){
-    //         if(i !== arr.length-1){
-    //             string.innerHTML += `${arr[i]}, `
-    //         }else{
-    //             string.innerHTML += `${arr[i]}  `
-    //         }
-    //     }
-    // }
-    
-    function StringId(){
-        const idArray = array.map(obj=>obj.id)
-        const arr = [...new Set(idArray)]
-        string.innerHTML = arr.join(', ')
+    for(let char of str){
+        if(!charMap[char]){
+            charMap[char] = 1
+        }else{
+            charMap[char] = charMap[char] + 1 || 1
+        }
     }
+  
 
-    StringId()
+    console.log(charMap)
